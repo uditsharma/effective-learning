ebcdccf1e018332f44de3dc481cd8f5bbb2fcbb5
Udit Sharma <udit.sharma@autodesk.com>
2018-03-28 06:31:36 +0530

    First commit

---
+++ algo/src/com/udit/geeks_for_geeks/graph/GraphWithAdjacencyList.java
@@ -0,0 +1,10 @@
+package com.udit.geeks_for_geeks.graph;
+
+public class GraphWithAdjacencyList {
+    public static void main(String[] args) {
+        int count = 0;
+        int total = count++ * 5;
+        total = count++ * 5;
+        System.out.println(total);
+    }
+}
